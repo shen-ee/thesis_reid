8e2f2357a96976bf4e13acc2cfc78f39d51d58f9
Shen Yi <jerry31@qq.com>
2018-05-03 16:20:48 +0800

    add distance ranking

--- LSH.py
+++ LSH.py
@@ -15,38 +15,57 @@ def trainLSH(X,LSHparam):
 
 def compressLSH(X,LSHparam):
     U = np.dot(X,LSHparam.w);
-    print("矩阵相乘结果为：")
-    print(U)
+    #print("矩阵相乘结果为：")
+    #print(U)
     U = U>0
-    print("二进制结果为：")
-    print(U.astype(int))
+    #print("二进制结果为：")
+    #print(U.astype(int))
+    return U.astype(int)
 
 def readFeature(filename):
     features = []
-    for line in open("queryfeature.txt"):
+    for line in open(filename):
         feature = []
         info = line.split(" ")[:2048]
         for item in info:
             feature.append(float(item))
         features.append(feature)
-    print(features)
+    #print(len(features))
     return features
 
+def dis(A,B):  # 马氏距离
+    C=[]
+    for i in range(len(A)):
+        C.append(A[i]^B[i])
+    return sum(C)
 
-X = [[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
-     [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,15],
-     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-     [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]]
+
 param = LSHparam()
-param.nbits = 8
+param.nbits = 512
 
-print("原向量维度为："+str(len(X[0])))
+querydata = readFeature("queryfeature.txt")
+
+trainLSH(querydata,param)
+
+query = compressLSH(querydata,param)
+
+testsetdata = readFeature("testfeature.txt")
+
+gallery = compressLSH(testsetdata,param)
+
+print("原向量维度为："+str(len(querydata[0])))
 print("目标向量维度为："+str(param.nbits))
 
-trainLSH(X,param)
-compressLSH(X,param)
+minnum = -1
+mindis = 99999
 
-readFeature("queryfeature.txt")
+for i,item in enumerate(gallery):
+    distance = dis(item,query[0])
+    if(distance<mindis):
+        minnum = i
+        mindis = distance
+    print(str(i)+":"+str(distance))
 
+print("号码为："+str(minnum))
 
 
